25c834b9740fb35f448e1dc6e4ccfaa9145f5c18
yangbowen <vanhexg@gmail.com>
2015-03-27 13:04:35 +0000

    test post 1

--- app/assets/javascripts/weibos.js.coffee
+++ app/assets/javascripts/weibos.js.coffee
@@ -3,8 +3,4 @@ $(document).ready ->
   state = $("state").html()
   console.log "code: "+code
   console.log "state:"+state
-  if (code && state)
-    console.log 'post'
-    $.post "https://api.weibo.com/oauth2/access_token", {clent_id: "2601417764", client_secret: "510fd9a175bc9f24d05514a6708c9517", grant_type: "authorization_code", code: "#{code}", redirect_uri: "http://i556.herokuapp.com/weibo"}, (data) ->
-      console.log "data: "+data 
     
--- app/controllers/weibos_controller.rb
+++ app/controllers/weibos_controller.rb
@@ -1,3 +1,26 @@
+require 'net/https'
+require 'uri'
+
+def post_api(api, args)
+    uri = URI.parse api
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req.set_form_data(args)
+    response = http.request(req)
+    JSON.load(response.body)
+end
+
+def get_api(api, args)
+    uri = URI.parse api
+    uri.query = args.collect { |a| "#{a[0]}=#{URI::encode(a[1].to_s)}"  }.join('&')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(req)
+    JSON.load(response.body)
+end
+
 class WeibosController < ApplicationController
     before_filter :authenticate_user!
     def index
@@ -6,6 +29,10 @@ class WeibosController < ApplicationController
       @appsecret = "510fd9a175bc9f24d05514a6708c9517"
       @state = params[:state]
       @code = params[:code]
+      if (@state && @code)
+        @data = post_api("https://api.weibo.com/oauth2/access_token", {:client_id => @appkey, :client_secret => @appsecret, :grant_type => "authorization_code", :redirect_uri => "http://i556.herokuapp.com/weibo", :code => @code})
+        @access_token = @data[:access_token]
+      end
     end
 
     def show
