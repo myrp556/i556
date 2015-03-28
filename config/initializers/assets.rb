a6eec046f999fcb017210ef01fd5a6ca7ae4973e
yangbowen <vanhexg@gmail.com>
2015-03-28 11:46:32 +0000

    add banner and img

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,7 +11,9 @@ Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( bootstrap.js )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 
+Rails.application.config.assets.precompile += %w( banner.css )
 Rails.application.config.assets.precompile += %w( home.js )
+Rails.application.config.assets.precompile += %w( home.css )
 Rails.application.config.assets.precompile += %w( login.css )
 
 Rails.application.config.assets.precompile += %w( weibos.js )
