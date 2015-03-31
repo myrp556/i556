7a2fd9e6d34abbea0f659e8bfac56b54c4ecb3cf
yangbowen <vanhexg@gmail.com>
2015-03-31 01:27:20 +0000

    fix length2

--- app/controllers/weibos_controller.rb
+++ app/controllers/weibos_controller.rb
@@ -113,7 +113,7 @@ class WeibosController < ApplicationController
               @keywords=[]
               @query = "http://api.yutao.us/api/keyword/<"
               @wb_msgs.each do |wb_msg|
-                if (@query.length > 3000)
+                if (@query.length > 1500)
                   res = get_words(@query+">")
                   res.each do |word|
                     if !@keywords.index(word)
